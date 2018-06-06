9b9bc02ac90d9d0a4076c71a2127ad8d0b0a769f
Gao Hong Jian <717373141@qq.com>
2018-06-06 15:45:22 +0800

    listview

--- app/src/main/java/com/bwei/day_02/MainActivity.java
+++ app/src/main/java/com/bwei/day_02/MainActivity.java
@@ -1,21 +1,29 @@
 package com.bwei.day_02;
 
+import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import adapter.MyListViewAdapter;
 import adapter.MyViewPager;
+import bean.Bean;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
 
-    private int[] img = {R.drawable.ph2,R.drawable.ph3,R.drawable.ph4,R.drawable.ph6};
+    private int[] img = {R.drawable.ph2, R.drawable.ph3, R.drawable.ph4, R.drawable.ph6};
+
+    private int[] age = {12, 23, 45, 12, 34, 12, 12, 12, 23, 34};
 
     private List<Integer> list = new ArrayList<>();
+
+    private List<Bean> ageList = new ArrayList<>();
+    private ListView mLvAge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,17 +45,31 @@ public class MainActivity extends AppCompatActivity {
             list.add(img[i]);
 
         }
+
+        for (int i = 0; i < age.length; i++) {
+            Bean bean = new Bean(age[i]);
+            ageList.add(bean);
+
+
+        }
+
         //设置适配器
         MyViewPager adapter = new MyViewPager(list, MainActivity.this);
         viewPager.setAdapter(adapter);
 
+        MyListViewAdapter adapter1 = new MyListViewAdapter(ageList, MainActivity.this);
+        mLvAge.setAdapter(adapter1);
+
     }
+
     //初始化视图
     private void initViews() {
         //获取资源ID
         viewPager = findViewById(R.id.viewpager);
+        mLvAge = findViewById(R.id.lv_age);
 
     }
+
     /**
      * 我处理了一个bug
      */
