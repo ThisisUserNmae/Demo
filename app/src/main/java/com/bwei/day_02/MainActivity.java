2236ca2a365163734967829813983df5496ac26c
liujiaan <965389477@qq.com>
2018-06-06 13:36:58 +0800

    viewPager

---
+++ app/src/main/java/com/bwei/day_02/MainActivity.java
@@ -0,0 +1,47 @@
+package com.bwei.day_02;
+
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+
+    private int[] img = {R.drawable.ph2,R.drawable.ph3,R.drawable.ph4,R.drawable.ph6};
+
+    private List<Integer> list = new ArrayList<>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+
+        initDatas();
+
+    }
+
+    private void initDatas() {
+
+        for (int i = 0; i < img.length; i++) {
+
+            list.add(img[i]);
+
+        }
+
+
+    }
+
+    private void initViews() {
+
+        viewPager = findViewById(R.id.viewpager);
+
+    }
+}
